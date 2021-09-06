64a3cf54b5dec90fdc5710ee3a957beaef6e1212
Vishal Patil <70944016+incognito1729@users.noreply.github.com>
2021-09-06 23:10:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React,{useState, useEffect} from 'react';
-import './App.css';
-import User from './Components/User';
-import UserSubmission from './Components/UserSubmission';
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Card from "./Components/Card";
+import User from "./Components/User";
+import UserSubmission from "./Components/UserSubmission";
 
 function App() {
   useEffect(() => {
-    document.title="Codeforces Crawler"
-  },[])
+    document.title = "Codeforces Crawler";
+  }, []);
   return (
-    <div className="App">
-      <User/>
-      <UserSubmission/>
-    </div>
+    <Card>
+      <div className="App">
+        <User />
+        <UserSubmission />
+      </div>
+    </Card>
   );
 }
 
