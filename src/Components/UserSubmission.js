824f7d6b560b91692d2415ec80552fea29b015a2
Vishal Patil <70944016+incognito1729@users.noreply.github.com>
2021-09-06 23:14:03 +0530

    Update UserSubmission.js

--- src/Components/UserSubmission.js
+++ src/Components/UserSubmission.js
@@ -1,44 +1,48 @@
-import React,{useState,useEffect} from 'react'
-import UserStat from './UserStat';
+import React, { useState, useEffect } from "react";
+import Card from "./Card";
+import UserStat from "./UserStat";
 
 function UserSubmission(prop) {
-    const [base,setBase]=useState('');
-    const [submission,setSubmission]=useState('');
-    const fetchData =async () => {
-        const response=await fetch(base);
-        const data=await response.json();
-        setSubmission(data);
-        console.log(data);
-        }
-        
-        useEffect(()=>{
-            if(base.length===0){
-                return;
-            }
-            fetchData();
-            
-        },[base]);
-        const submitHandler=(e)=>{
-            // console.log(handle);
-            e.preventDefault();
-            setBase(`https://codeforces.com/api/user.status?handle=${username}`);        
-            SetUsername('');
-            
-            
-    }
-    const [username,SetUsername]=useState('')
-    return (
-            
-            
-        <>
-            <form onSubmit={submitHandler}>
-                <input type="text" placeholder="username" value={username} onChange={(e)=>{SetUsername(e.target.value)}}/>
-            </form>
+  const [base, setBase] = useState("");
+  const [submission, setSubmission] = useState("");
+  const fetchData = async () => {
+    const response = await fetch(base);
+    const data = await response.json();
+    setSubmission(data);
+    console.log(data);
+  };
 
-            {submission['status']==='OK'&&<UserStat submission={submission}/>}
-        </>
-    
-    )
+  useEffect(() => {
+    if (base.length === 0) {
+      return;
+    }
+    fetchData();
+  }, [base]);
+  const submitHandler = (e) => {
+    // console.log(handle);
+    e.preventDefault();
+    setBase(`https://codeforces.com/api/user.status?handle=${username}`);
+    // SetUsername("");
+  };
+  const [username, SetUsername] = useState("");
+  return (
+    <Card>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          placeholder="username"
+          value={username}
+          onChange={(e) => {
+            SetUsername(e.target.value);
+          }}
+        />
+      </form>
+
+      {submission["status"] === "OK" && (
+        <UserStat submission={submission} username={username} />
+      )}
+    </Card>
+  );
 }
 
 export default UserSubmission;
